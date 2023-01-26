b8f87097f1f86329a051aaa44e07c4094e49ed17
Dokji <114224307+Dokji@users.noreply.github.com>
2023-01-26 17:51:31 +0200

    Add files via upload

--- src/domain/Customer.java
+++ src/domain/Customer.java
@@ -1,5 +1,23 @@
 package domain;
 public class Customer {
+    public int getID() {
+        return ID;
+    }
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+    public boolean isIsNew() {
+        return isNew;
+    }
+    public void setIsNew(boolean isNew) {
+        this.isNew = isNew;
+    }
+    public float getTotal() {
+        return total;
+    }
+    public void setTotal(float total) {
+        this.total = total;
+    }
     public int ID = 1;
     public boolean isNew = true;
     public float total = 1000;
@@ -9,4 +27,3 @@ System.out.println("ID: "+ID);
 System.out.println("isNew: "+isNew);
 System.out.println("total: "+total);
  }}
-
