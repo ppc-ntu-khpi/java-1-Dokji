3a7db7d6027f113dc4de02bd3523f8259077f433
Dokji <114224307+Dokji@users.noreply.github.com>
2023-01-26 14:15:02 +0200

    Add files via upload

---
+++ src/domain/Customer.java
@@ -0,0 +1,12 @@
+package domain;
+public class Customer {
+    public int ID = 1;
+    public boolean isNew = true;
+    public float total = 1000;
+    
+ public void displayCustomerInfo(){
+System.out.println("ID: "+ID);
+System.out.println("isNew: "+isNew);
+System.out.println("total: "+total);
+ }}
+
